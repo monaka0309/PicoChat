cb01775260da55963f476ee28462b7cd0076e154
nakaryunosuke <naka.sava0309@gmail.com>
2024-12-20 17:06:04 +0900

    first commit

---
+++ backend/pico-chat/src/main/java/com/example/pico_chat/PicoChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.pico_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicoChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicoChatApplication.class, args);
+	}
+
+}
